2f2f2927d2469b441f7c1fb4324000837857a092
S <khaleelnikhath@gmail.com>
2018-01-20 18:26:19 +0000

    first commit

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class TestBase {
+
+	public static void testbase(){
+		System.out.println("Printing testbase");
+	}
+}
---
+++ src/main/java/com/qa/util/util.java
@@ -0,0 +1,8 @@
+package com.qa.util;
+
+public class util {
+	public static void util(){
+		System.out.println("Print Util");
+	}
+
+}
