402f28cb2fbd1038e781a60410d5e0c00c40d52d
S <khaleelnikhath@gmail.com>
2018-01-22 07:12:39 +0000

    committing the changes

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,5 +4,9 @@ public class LoginPage {
 	public void login(){
 		System.out.println("logging in");
 	}
+	
+	public void login2(){
+		System.out.println("logging2 in");
+	}
 
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class SearchPage {
+   public static void search(){
+	   System.out.println("Search page");
+   }
+}
