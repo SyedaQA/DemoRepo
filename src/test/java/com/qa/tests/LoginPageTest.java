17692b0d4ca1f26f289c17dce81b30498b489ff7
S <khaleelnikhath@gmail.com>
2018-01-24 10:49:43 +0000

    new

--- src/test/java/com/qa/tests/LoginPageTest.java
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -1,9 +1,12 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 /*
  * 
  * *.author name Syeda
  */
+@Test
 public class LoginPageTest {
 	public static void loginTest() {
 		System.out.println("Print loginpage Test");
